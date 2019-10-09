a3e1c1fb1b8b246c3461925da77f077a86bd2009
FALISE Thibault <thibault-externe.falise@edf.fr>
2019-10-09 17:27:17 +0200

    Init projects

---
+++ Sources/TalentAgileShop.Cart/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TalentAgileShop.Cart
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sources/TalentAgileShop.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TalentAgileShop.Model
+{
+    public class Class1
+    {
+    }
+}
